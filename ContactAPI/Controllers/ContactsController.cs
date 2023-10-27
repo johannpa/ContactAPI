0690189cd766594b61a1d4e49762ad765f534a19
johannpa <johann_paque@hotmail.fr>
2023-10-27 18:13:36 +0200

    Create the controller and the first method GetContacts

---
+++ ContactAPI/Controllers/ContactsController.cs
@@ -0,0 +1,23 @@
+﻿using ContactAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+        private readonly ContactsAPIDbContext _context;
+
+        public ContactsController(ContactsAPIDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetContacts()
+        {
+            return Ok(_context.Contacts.ToList());
+        }
+    }
+}
